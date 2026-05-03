5cd5d5bf9247b023cf2c00bd0f9c7cf942d1bec0
Devyani Patil <dp1062445@gmail.com>
2026-05-03 13:41:28 +0530

    updated the port as per railway

--- server/server.js
+++ server/server.js
@@ -22,6 +22,6 @@ app.get("/", (req, res) => {
   res.send("API Running 🚀");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
